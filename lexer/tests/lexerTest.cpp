bcc692623dc0715acf8a4fd604f86c06db3e4c01
shejialuo <shejialuo@gmail.com>
2023-02-12 14:57:40 +0800

    feat: add tests file for more lexer test

--- lexer/tests/lexerTest.cpp
+++ lexer/tests/lexerTest.cpp
@@ -49,3 +49,49 @@ TEST(Lexer, TestNextToken) {
     }
   }
 }
+
+TEST(Lexer, TestNextTokenComplicated) {
+  std::string input{"let five = 5; \
+                        let ten = 10; \
+                        \
+                        let add = fn(x, y) {\
+                          x + y; \
+                        }; \
+                        \
+                        let result = add(five, ten);"};
+
+  std::vector<TestToken> tests{
+      {TokenTypes::LET, "let"},      {TokenTypes::IDENT, "five"},  {TokenTypes::ASSIGN, "="},
+      {TokenTypes::INT, "5"},        {TokenTypes::SEMICOLON, ";"}, {TokenTypes::LET, "let"},
+      {TokenTypes::IDENT, "ten"},    {TokenTypes::ASSIGN, "="},    {TokenTypes::INT, "10"},
+      {TokenTypes::SEMICOLON, ";"},  {TokenTypes::LET, "let"},     {TokenTypes::IDENT, "add"},
+      {TokenTypes::ASSIGN, "="},     {TokenTypes::FUNCTION, "fn"}, {TokenTypes::LPAREN, "("},
+      {TokenTypes::IDENT, "x"},      {TokenTypes::COMMA, ","},     {TokenTypes::IDENT, "y"},
+      {TokenTypes::RPAREN, ")"},     {TokenTypes::LBRACE, "{"},    {TokenTypes::IDENT, "x"},
+      {TokenTypes::PLUS, "+"},       {TokenTypes::IDENT, "y"},     {TokenTypes::SEMICOLON, ";"},
+      {TokenTypes::RBRACE, "}"},     {TokenTypes::SEMICOLON, ";"}, {TokenTypes::LET, "let"},
+      {TokenTypes::IDENT, "result"}, {TokenTypes::ASSIGN, "="},    {TokenTypes::IDENT, "add"},
+      {TokenTypes::LPAREN, "("},     {TokenTypes::IDENT, "five"},  {TokenTypes::COMMA, ","},
+      {TokenTypes::IDENT, "ten"},    {TokenTypes::RPAREN, ")"},    {TokenTypes::SEMICOLON, ";"},
+      {TokenTypes::_EOF, ""},
+  };
+
+  Lexer l{input};
+
+  for (int i = 0; i < tests.size(); ++i) {
+    Token token = l.nextToken();
+
+    TestToken &testToken = tests[i];
+
+    if (token.Type != testToken.expectedType) {
+      spdlog::error("test[{}] - token type wrong. expected='{}', got='{}'", i, testToken.expectedType, token.Type);
+      FAIL();
+    }
+
+    if (token.Literal != testToken.expectedLiteral) {
+      spdlog::error(
+          "test[{}] - token literal wrong. expected='{}', got='{}'", i, testToken.expectedLiteral, token.Literal);
+      FAIL();
+    }
+  }
+}
